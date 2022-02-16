be4144b5c8fd1510603b339be4e481263816a38c
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-02-16 14:17:26 +0000

    verified transactions

--- newProject/complete.js
+++ newProject/complete.js
@@ -45,7 +45,7 @@ class Chain{
 
   constructor(){
     //create the genesis block
-    this.chain = [new Block(null,new Transaction('genesis','satoshi',100))];
+    this.chain = [new Block(null,new Transaction('genesis','satoshi',500))];
     
     this.difficulty = 4;
   }
@@ -66,7 +66,7 @@ class Chain{
   getBalanceOfAddress(address){
     let balance = 0;
     for(const block of this.chain){
-      const trans = block.transaction
+      const trans = block.transaction;
       if(trans.payer === address){
         balance -= trans.amount;
       }
@@ -83,12 +83,26 @@ class Wallet{
     
   constructor(publicKey){
     this.publicKey = publicKey;
+    this.minimum = 100;
   }
 
   sendMoney(amount,payeePublicKey){
-    const transaction = new Transaction(this.publicKey,payeePublicKey,amount);                
-    Chain.instance.addBlock(transaction);
-    console.log(transaction);
+    let minimum = this.minimum;
+    let availableBal = Chain.instance.getBalanceOfAddress(this.publicKey);
+    
+    if(availableBal > 0 && availableBal >= amount){
+      if(amount >= minimum){
+        const transaction = new Transaction(this.publicKey,payeePublicKey,amount);                
+       Chain.instance.addBlock(transaction);
+       console.log(transaction);
+      }else{
+        console.log(`\nunable to initiate transaction from ${this.publicKey}...minimum transactable amount is ${minimum}`);
+      }
+      //console.log(Chain.instance.getBalanceOfAddress(this.publicKey) <= 0);
+    }else{
+      console.log("\ninsufficient funds to initiate transaction from",this.publicKey);
+    }
+    
   }
 }
 
@@ -96,10 +110,14 @@ const satoshi = new Wallet('satoshi');
 const bob = new Wallet('bob');
 const alice = new Wallet('alice');
 
-satoshi.sendMoney(50,bob.publicKey);
-bob.sendMoney(35,alice.publicKey);
-alice.sendMoney(57,bob.publicKey);
+satoshi.sendMoney(200,bob.publicKey);
+bob.sendMoney(100,alice.publicKey);
+alice.sendMoney(101,bob.publicKey);
 
 console.log(Chain.instance);
 
 console.log("Balance of bob is ",Chain.instance.getBalanceOfAddress(bob.publicKey));
+
+console.log("Balance of alice is ",Chain.instance.getBalanceOfAddress(alice.publicKey));
+
+console.log("Balance of satoshi is ",Chain.instance.getBalanceOfAddress(satoshi.publicKey));
